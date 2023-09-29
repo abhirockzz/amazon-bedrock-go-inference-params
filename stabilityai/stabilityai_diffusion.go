0bc341d75f9a737d65283a502906ddf69a7e5899
Abhishek <abhirockzz@gmail.com>
2023-09-30 00:49:07 +0530

    add stabilityai response model

--- stabilityai/stabilityai_diffusion.go
+++ stabilityai/stabilityai_diffusion.go
@@ -1,5 +1,7 @@
 package stabilityai
 
+import "encoding/base64"
+
 type Request struct {
 	TextPrompts []TextPrompt `json:"text_prompts"`
 	CfgScale    float64      `json:"cfg_scale"`
@@ -9,4 +11,22 @@ type Request struct {
 
 type TextPrompt struct {
 	Text string `json:"text"`
+}
+
+type Response struct {
+	Result    string     `json:"result"`
+	Artifacts []Artifact `json:"artifacts"`
+}
+
+type Artifact struct {
+	Base64       string `json:"base64"`
+	FinishReason string `json:"finishReason"`
+}
+
+func (a *Artifact) DecodeImage() ([]byte, error) {
+	decoded, err := base64.StdEncoding.DecodeString(a.Base64)
+	if err != nil {
+		return nil, err
+	}
+	return decoded, nil
 }
