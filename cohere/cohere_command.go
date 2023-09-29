69e6dac84d364e12ed2240460c59d985cd9fb150
Abhishek <abhirockzz@gmail.com>
2023-09-30 00:18:51 +0530

    update cohere model

--- cohere/cohere_command.go
+++ cohere/cohere_command.go
@@ -6,10 +6,10 @@ type Request struct {
 	P                 float64          `json:"p"`
 	K                 float64          `json:"k"`
 	MaxTokens         int              `json:"max_tokens"`
-	StopSequences     []string         `json:"stop_sequences"`
-	ReturnLikelihoods ReturnLikelihood `json:"return_likelihoods"`
-	Stream            bool             `json:"stream"`
-	NumGenerations    int              `json:"num_generations"`
+	StopSequences     []string         `json:"stop_sequences,omitempty"`
+	ReturnLikelihoods ReturnLikelihood `json:"return_likelihoods,omitempty"`
+	Stream            bool             `json:"stream,omitempty"`
+	NumGenerations    int              `json:"num_generations,omitempty"`
 }
 
 type ReturnLikelihood string
