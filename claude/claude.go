71c3a5098f540a5db5aa73c1499d5fd4477414c4
Abhishek <abhirockzz@gmail.com>
2023-09-29 23:54:13 +0530

    update model

--- claude/claude.go
+++ claude/claude.go
@@ -2,7 +2,7 @@ package claude
 
 type Request struct {
 	Prompt            string   `json:"prompt"`
-	MaxTokensToSample int      `json:"max_tokens_to_sample,omitempty"`
+	MaxTokensToSample int      `json:"max_tokens_to_sample"`
 	Temperature       float64  `json:"temperature,omitempty"`
 	TopP              float64  `json:"top_p,omitempty"`
 	TopK              int      `json:"top_k,omitempty"`
