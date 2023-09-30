df9d7267d6195ccec1e9d475b41dbea3b50d6c30
Abhishek <abhirockzz@gmail.com>
2023-09-30 10:36:35 +0530

    update titan embedding

--- amazontitan/embedding/amazon_titan_embed.go
+++ amazontitan/embedding/amazon_titan_embed.go
@@ -3,3 +3,8 @@ package embedding
 type Request struct {
 	InputText string `json:"inputText"`
 }
+
+type Repsponse struct {
+	Embedding           []float64 `json:"embedding"`
+	InputTextTokenCount int       `json:"inputTextTokenCount"`
+}
